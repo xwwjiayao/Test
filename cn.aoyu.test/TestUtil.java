c598f094ec645b2cac5b1c275dc08af873e77d5a
xwwjiayao <654618347@qq.com>
2017-04-26 20:01:08 +0800

    提交项目

---
+++ cn.aoyu.test/TestUtil.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/4/26.
+ */
+public class TestUtil {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+
+}
